a3584958715a3e55d2a7a4f04a9eb579857db365
Vixingo <nazmul.abbu@gmail.com>
2025-06-12 12:40:56 +0600

    k

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./*.html",
-    "./**/*.html",
-    "./*.js",
-    "./**/*.js"
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#003E92',
-        secondary: '#FF9D00',
-      },
+    content: ["./*.html", "./**/*.html", "./*.js", "./**/*.js"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#003E92",
+                secondary: "#FF9D00",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
